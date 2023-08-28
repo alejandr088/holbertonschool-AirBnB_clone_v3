7f477313ca33ea5266121c7f4a1d6dd16649df68
alejandr088 <alejandrivello@gmail.com>
2023-08-27 22:30:49 -0700

    tasks 11,12 & 13

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,8 +4,10 @@ from flask import Flask
 from api.v1.views import app_views
 from models import storage
 from os import getenv
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 
 app.register_blueprint(app_views)
 
