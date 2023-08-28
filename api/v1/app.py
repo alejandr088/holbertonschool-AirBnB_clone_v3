85e5d861d77f063874fdcfdaeb3f31ed8aee4c97
alejandr088 <alejandrivello@gmail.com>
2023-08-27 22:11:43 -0700

    fixed task 7, added tasks 8,9 & 10

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,11 +3,11 @@
 from flask import Flask
 from api.v1.views import app_views
 from models import storage
-import os
+from os import getenv
 
 app = Flask(__name__)
 
-app.register_blueprint(app_views, url_prefix='/api/v1')
+app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
@@ -19,10 +19,10 @@ def teardown_db(exception):
 @app.errorhandler(404)
 def page_not_found(error):
     """ 404 page not found """
-    return {'error': 'Not found'}, 404
+    return ({'error': 'Not found'}, 404)
 
 
 if __name__ == "__main__":
-    host = os.environ.get('HBNB_API_HOST', '0.0.0.0')
-    port = os.environ.get('HBNB_API_PORT', '5000')
+    host = getenv('HBNB_API_HOST', '0.0.0.0')
+    port = int(getenv('HBNB_API_PORT', 5000))
     app.run(host=host, port=port, threaded=True)
--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -7,16 +7,15 @@ from models import storage
 from models.state import State
 
 
-@app_views.route('/states', methods=['GET'])
-@app_views.route('/states/', methods=['GET'])
+@app_views.route('/states', methods=['GET'], strict_slashes=False)
 def get_states():
     """Retrieves the list of all State objects"""
     states = storage.all(State).values()
-    state_list = [state.to_dict() for state in states.values()]
+    state_list = [state.to_dict() for state in states]
     return jsonify(state_list)
 
 
-@app_views.route('/states/<state_id>', methods=['GET'])
+@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
 def get_state(state_id):
     """Retrieves a State object"""
     state = storage.get(State, state_id)
@@ -25,7 +24,8 @@ def get_state(state_id):
     return jsonify(state.to_dict())
 
 
-@app_views.route('/states/<state_id>', methods=['DELETE'])
+@app_views.route('/states/<state_id>',
+                 methods=['DELETE'], strict_slashes=False)
 def delete_state(state_id):
     """Delete a State object"""
     state = storage.get(State, state_id)
@@ -33,10 +33,10 @@ def delete_state(state_id):
         abort(404)
     storage.delete(state)
     storage.save()
-    return (jsonify({}), 200)
+    return jsonify({}), 200
 
 
-@app_views.route('/states', methods=['POST'])
+@app_views.route('/states', methods=['POST'], strict_slashes=False)
 def create_state():
     """Creates a State"""
     data = request.get_json()
@@ -47,10 +47,10 @@ def create_state():
     new_state = State(**data)
     storage.new(new_state)
     storage.save()
-    return (jsonify(new_state.to_dict()), 201)
+    return jsonify(new_state.to_dict()), 201
 
 
-@app_views.route('/states/<state_id>', methods=['PUT'])
+@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
 def update_state(state_id):
     """Updates a State object"""
     state = storage.get(State, state_id)
